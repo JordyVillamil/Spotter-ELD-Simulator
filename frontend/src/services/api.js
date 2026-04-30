1e9a2980346f3b5634ad52379679129041753576
Jordy Villamil <jordyvillamilletras@gmail.com>
2026-04-30 16:32:16 -0500

    new deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://spotter-eld-simulator.onrender.com';
+const API_BASE_URL = 'https://spotter-eld-simulator.onrender.com/api/v1/';
+//const API_BASE_URL = 'http://127.0.0.1:8000/api/v1';
+
 
 
 export const calculateELD = async (data) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/api/v1/calculate-eld/`, data);
+    const response = await axios.post(`${API_BASE_URL}/calculate-eld/`, data);
     return response.data;
   } catch (error) {
     console.error("Error calling ELD API:", error);
