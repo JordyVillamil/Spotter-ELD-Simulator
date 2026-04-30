46b8aba0ea7f9d47e2ab77170230cec0f57286e4
Jordy Villamil <jordyvillamilletras@gmail.com>
2026-04-30 10:33:55 -0500

    render url update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://spottereldsimulator-ke9l.vercel.app/api/v1';
+const API_BASE_URL = 'https://spotter-eld-simulator.onrender.com/api/v1';
 
 export const calculateELD = async (data) => {
   try {
