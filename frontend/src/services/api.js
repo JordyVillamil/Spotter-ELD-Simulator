5d783a17753e190a2d832ce3441f850ec537c7a9
Jordy Villamil <jordyvillamilletras@gmail.com>
2026-04-30 23:08:39 -0500

    fix issues with docker´s container

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://spotter-eld-simulator.onrender.com/api/v1/';
-//const API_BASE_URL = 'http://127.0.0.1:8000/api/v1';
-
-
+// Base URL apuntando a producción en Render
+const API_BASE_URL = 'https://spotter-eld-simulator.onrender.com/api/v1';
+// const API_BASE_URL = 'http://127.0.0.1:8000/api/v1';
 
 export const calculateELD = async (data) => {
   try {
+    // Usamos template literals (comillas invertidas) para unir la base con la ruta final
     const response = await axios.post(`${API_BASE_URL}/calculate-eld/`, data);
     return response.data;
   } catch (error) {
